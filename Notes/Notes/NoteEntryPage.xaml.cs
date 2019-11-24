e639c9afcf84c58273e6ebeb89149ddb7b24f239
Kehinde Adejoro <kennyt4real.ak@gmail.com>
2019-11-24 08:25:08 +0100

    Add project files.

---
+++ Notes/Notes/NoteEntryPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Notes.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+        async void OnSaveButtonClicked(object sender,EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            if (string.IsNullOrWhiteSpace(note.FileName))
+            {
+                // Save
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.text");
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                // Update
+                File.WriteAllText(note.FileName, note.Text);
+            }
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender,EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            if (File.Exists(note.FileName))
+            {
+                File.Delete(note.FileName);
+            }
+            await Navigation.PopAsync();
+        }
+    }
+}
